a5220716ab761f9ff6478dbe98033fbf346699ab
plstravels00-byte <plstravels00@gmail.com>
2025-11-12 17:29:21 +0530

    Update DriverWallet.js

--- models/DriverWallet.js
+++ models/DriverWallet.js
@@ -1,37 +1,36 @@
 import mongoose from "mongoose";
 
-const driverWalletSchema = new mongoose.Schema({
-  driverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Driver",
-    required: true,
+const walletSchema = new mongoose.Schema(
+  {
+    driverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    branchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Branch",
+    },
+    amount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reason: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    addedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  branchId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Branch",
-  },
-  amount: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  reason: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "approved", "rejected"],
-    default: "pending",
-  },
-  addedBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Manager or Admin
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
-export default mongoose.model("DriverWallet", driverWalletSchema);
+export default mongoose.model("DriverWallet", walletSchema);
